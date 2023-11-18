2bb4b7ea50a350d8cbdf0b9e2f23df2d80450744
Forhad5194 <forhadhossain5194@gmail.com>
2023-11-18 14:40:51 +0600

    Data Load it Working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -15,8 +16,8 @@ app.use(express.json());
 
 // Mongodb 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://FootUser:<password>@cluster0.gliiomq.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gliiomq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,12 +32,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("Menu").collection("Menu");
+    const reviewCollection = client.db("review").collection("review");
+    
+
+    app.get('/menu', async(req, res) =>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+    app.get('/review', async(req, res) =>{
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
